0f8bcf03bd8f691926a2e01f0b2db967604a005c
Antohmullih-2020 <antohmulli@outlook.com>
2021-03-29 20:03:52 +0300

    uploads

---
+++ MVC-SETUP/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_SETUP.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with id ={id}";
+        }
+        public string searchbook (string bookname, string authorname)
+        {
+            return $"Book with Name ={bookname} &  Author= {authorname}";
+        }
+    }
+}
