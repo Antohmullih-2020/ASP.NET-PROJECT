7d13c698f4a455c3346344620acb45992a108da8
Antohmullih-2020 <antohmulli@outlook.com>
2021-04-02 11:45:46 +0300

    Razors

--- MVC-SETUP/Controllers/HomeController.cs
+++ MVC-SETUP/Controllers/HomeController.cs
@@ -8,12 +8,23 @@ namespace MVC_SETUP.Controllers
 {
     public class HomeController : Controller
     {
+        //returning a model using view
+        //returning a mode that as same name for method action and file name
         public ViewResult index()
         {
-            return View();
+            var student = new { id = 1, Name = "Antony" };
+            return View(student);
         }
 
+        //returning a model using view
+        //Action method with different name with file name
         public ViewResult About_us()
+        {
+            var student = new { id = 1, name = "Antony" };
+            return View("Index", student);
+        }
+
+        public ViewResult Contact_us()
         {
             return View();
         }
