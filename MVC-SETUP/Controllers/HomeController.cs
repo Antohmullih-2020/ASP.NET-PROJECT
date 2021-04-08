2b5a29442e40a04b51f9d51ed9627f0944729306
Antohmullih-2020 <antohmulli@outlook.com>
2021-04-08 19:42:26 +0300

    ViewData

--- MVC-SETUP/Controllers/HomeController.cs
+++ MVC-SETUP/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_SETUP.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Dynamic;
+
 
 namespace MVC_SETUP.Controllers
 {
@@ -13,15 +14,12 @@ namespace MVC_SETUP.Controllers
         //returning a mode that as same name for method action and file name
         public ViewResult index()
         {
-            
-            ViewBag.Title ="Santozpalmer web";
-            dynamic data = new ExpandoObject();
-            data.id = 123;
-            data.name = "Palmer";
-            ViewBag.Data = data;
+            ViewData["property1"] = "Santos";
+            ViewData["book"] = new Bookmodel() { Author = "kiptoo", Id = 1 };
+ 
             return View();
         }
-
+         
         //returning a model using view
         //Action method with different name with file name
         public ViewResult About_us()
