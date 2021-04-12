1761427a1ed3f6ea8a3d6e5a6bcd480136302dc7
Antohmullih-2020 <antohmulli@outlook.com>
2021-04-12 22:27:07 +0300

    db

---
+++ MVC-SETUP/Data/BookStorecontext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_SETUP.Data
+{
+    public class BookStorecontext : DbContext
+    {
+        public BookStorecontext(DbContextOptions<BookStorecontext> options)
+            :base(options)
+        {
+
+        }
+
+        //setting db
+        public DbSet<Books> Books { get; set; }
+
+        //db setup
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Sever.;DatabaseBookStore;Integrated Security=True;")
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
